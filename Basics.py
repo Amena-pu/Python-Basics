26b1e658521611d3242837fa5a8e22588cca53f1
Amena Akter <61085074+Amena-pu@users.noreply.github.com>
2021-05-06 21:02:58 +0600

    Add files via upload

---
+++ Basics.py
@@ -0,0 +1,14 @@
+def main():
+    x=int(input ())
+    y =int(input())
+
+    if (x < y):
+        st = "x is less than y"
+    else:
+        st = "x is greater than y"
+
+    print (st)
+
+
+if __name__ == "__main__":
+    main()
