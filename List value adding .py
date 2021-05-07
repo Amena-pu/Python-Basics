03f62b6d99af389377911e8677c3c834f3f4ea1b
Amena Akter <61085074+Amena-pu@users.noreply.github.com>
2021-05-07 10:43:15 +0600

    Add files via upload

---
+++ List value adding .py
@@ -0,0 +1,18 @@
+def insertion_sort(list):
+    for index in range(1,len(list)):
+        value=list[index]
+        i=index-1
+        while i>=0:
+            if value < list[i]:
+                list [i+1] = list[i]
+                list[i]=value
+                i=i-1
+            else:
+                break
+a =[5,2,4,6,1,3]
+insertion_sort(a)
+print(a)
+sum=0
+for val in a:
+ sum = sum+val
+print(sum)
