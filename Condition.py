931811759850411f2d50f4dd6d8ff818ae38d0d0
Amena Akter <61085074+Amena-pu@users.noreply.github.com>
2021-05-07 20:28:17 +0600

    Update Condition.py

--- Condition.py
+++ Condition.py
@@ -2,14 +2,10 @@ y=int(input())
 x=int(input())
 numbers = [1, 2, 3, 4, 5, 1, 4, 5, 6, 7, 98, 56]
 Sum = sum(numbers)
-odd_square = x**2
-print ("Odd square %i"%odd_square)
+print(Sum)
+odd_square = [x**2 for x in range(1,11) if x%2==1]
+print (odd_square)
 if len(numbers)%2==0:
     print("Python is easy")
 else:
     print("Python is hard")
-sum1 =odd_square
-for b in range(1,len(numbers)):
-    val= (b*odd_square)
-    print(" %i\n"%val)
-
