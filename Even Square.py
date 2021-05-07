c7ca6e95f435ff0cf02e98d278057f7f143fac88
Amena Akter <61085074+Amena-pu@users.noreply.github.com>
2021-05-07 20:37:23 +0600

    User Input and even square

---
+++ Even Square.py
@@ -0,0 +1,8 @@
+x=int(input())
+
+even_square = [x**2 for x in range(1,20) if x%2==0]
+print (even_square)
+if x%2==0:
+    print("Python is favourite language")
+else:
+    print("I don't like python")
