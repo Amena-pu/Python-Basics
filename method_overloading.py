26634b00425af9bf0698b0484d99363d0b4b95d3
Amena Akter <61085074+Amena-pu@users.noreply.github.com>
2021-06-19 09:15:36 +0600

    Overloading.

---
+++ method_overloading.py
@@ -0,0 +1,25 @@
+z=3
+u = "string"
+
+def main():
+    x=int(input ())
+    y =int(input())
+
+    z = x+y
+
+    print(z)
+
+def cap():
+    
+    global u 
+    a = u.capitalize()
+    print(a)
+    b = "Acknowledge"
+    c =b.casefold()
+    print(c)
+    global z
+    v = z+5
+    print(v) 
+if __name__ == "__main__":
+    main()
+    cap()
